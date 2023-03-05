75a26f32d33a13872f11a0ee1aee833aaa3bd163
Andrew Mo <106348010+theandrewmo@users.noreply.github.com>
2023-03-05 16:05:32 -0700

    added sessions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash 
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import satisfaction_survey
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -8,24 +8,27 @@ app.config['SECRET_KEY'] = 'oh-so-secret'
 debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-responses = []
-
 @app.route('/')
 def index():
     """ Homepage """
     return render_template('start.html', survey=satisfaction_survey)
 
+@app.route('/setsession', methods=["POST"])
+def set_session():
+    session['responses'] = []
+    return redirect('/questions/0')
+
 @app.route('/questions/<int:qindex>')
 def show_question(qindex):
     """ Shows each question page """
     
-    pindex = len(responses)
+    pindex = len(session['responses'])
     if qindex is pindex and pindex is not 4:
         return render_template('question.html', survey=satisfaction_survey, qindex=qindex)
     elif pindex is 4:
         return redirect(f'/thanks')
     else:
-        flash(f"Redirect to question {pindex}: you tried to access an invaid question out of order")
+        flash(f"Redirect to question {pindex}: you tried to access an invalid question out of order")
         return redirect(f'/questions/{pindex}')
 
 @app.route('/answer')
@@ -38,9 +41,9 @@ def handle_thanks():
 def handle_answer(aindex):
     """ Handles answer """
 
+    responses = session['responses']
     responses.append(request.form['choice'])
-    print('the length of responses is' + str(len(responses)))
-    print(responses)
+    session['responses'] = responses
     if aindex < len(satisfaction_survey.questions) - 1:
         aindex = aindex + 1
         return redirect(f'/questions/{aindex}')
